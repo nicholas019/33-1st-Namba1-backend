5d912f005677254883cfb88d76edc625088e3dca
sorkihg <sorkihg@gmail.com>
2022-05-26 21:15:20 +0900

    [Namba1팀]backend product-theme 작업중

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+from .models import Product
+
+class ProductView(View):
+    def get(request):
+        products = Product.objects.filter(theme=request.theme)
+        product_list = [{
+                    'id'         : product.id,
+                    'name'      : product.title,
+                    'serving'    : product.content,
+                    'cookTime'  : product.image_url,
+                    'price'  : product.image_url,
+                } for product in products]
+        
+        return JsonResponse({'posting': product_list, 'message': 'SUCCESS'}, status=200)
