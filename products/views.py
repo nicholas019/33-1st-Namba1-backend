846285ceb05fe7b31589efb591df17c250a6e987
sorkihg <sorkihg@gmail.com>
2022-05-27 21:41:32 +0900

    [Namba1팀]backend ProductThemeView API

--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+from .models import Product, Theme
+
+# product 
+class ProductThemeView(View):
+    def get(self, request):
+        theme1 = request.GET.get('theme', None)
+        sort = request.GET.get('sort', 'id')
+
+        sort_type= {
+            "신메뉴순" : "-id",
+            "높은가격순": "price",
+            "낮은가격순": "-price"
+        }
+
+        themes = Theme.objects.get(theme=theme1).id
+        products = Product.objects.filter(producttheme__theme_id = themes).order_by(sort_type[sort])
+        product_list = [{
+            "id"      : product.id,
+            "name"    : product.name,
+            "serving" : product.serving,
+            "cookTime": product.cook_time,
+            "prepTime": product.prep_time,
+            "price"   : product.price,
+            # "image"   : image,
+        } for product in products]
+        return JsonResponse({'posting': product_list, 'message': 'SUCCESS'}, status=200)
