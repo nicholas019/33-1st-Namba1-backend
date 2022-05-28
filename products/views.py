4bdb131b7679642b8c1d7a6f1d5c2ea7d17441e3
sorkihg <sorkihg@gmail.com>
2022-05-28 14:45:39 +0900

    [Namba1팀]backend ProductListlView 수정1

--- products/views.py
+++ products/views.py
@@ -6,25 +6,28 @@ from .models import Product, Theme
 
 class ProductListView(View):
     def get(self, request):
-        theme = request.GET.get('theme', "KIDS")
-        sort = request.GET.get('sort', "신메뉴순")
+        try:
+            theme = request.GET.get('theme', "KIDS")
+            sort = request.GET.get('sort', "신메뉴순")
 
-        sort_type= {
-            "신메뉴순" : "-id",
-            "높은가격순": "-price",
-            "낮은가격순": "price"
-        }
+            sort_type= {
+                "신메뉴순" : "-id",
+                "높은가격순": "-price",
+                "낮은가격순": "price"
+            }
 
-        themes = Theme.objects.get(theme=theme).id
-        products = Product.objects.filter(producttheme__theme_id = themes).order_by(sort_type[sort])
-        product_list = [{
-            "id"      : product.id,
-            "name"    : product.name,
-            "serving" : product.serving,
-            "cookTime": product.cook_time,
-            "prepTime": product.prep_time,
-            "price"   : product.price,
-            "spice"   : [spice.level for spice in product.spice_set.all()],
-            "image"   : [image.image for image in product.productimage_set.all()][0]
-        } for product in products]
-        return JsonResponse({'product_list': product_list, 'message': 'SUCCESS'}, status=200)
+            themes = Theme.objects.get(theme=theme).id
+            products = Product.objects.filter(producttheme__theme_id = themes).order_by(sort_type[sort])
+            product_list = [{
+                "id"      : product.id,
+                "name"    : product.name,
+                "serving" : product.serving,
+                "cookTime": product.cook_time,
+                "prepTime": product.prep_time,
+                "price"   : product.price,
+                "spice"   : [spice.level for spice in product.spice_set.all()],
+                "image"   : [image.image for image in product.productimage_set.all()][0]
+            } for product in products]
+            return JsonResponse({'product_list': product_list, 'message': 'SUCCESS'}, status=200)
+        except KeyError:
+            return JsonResponse({"message": 'KeyError'}, status = 400)  
