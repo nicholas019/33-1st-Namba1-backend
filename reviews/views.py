d41483d3f47a08d46281565abec43fb7a04d002f
sorkihg <sorkihg@gmail.com>
2022-05-30 22:45:41 +0900

    [Namba1팀]backend reivew

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
 
-# Create your views here.
+import json
+
+from django.views import View
+
+from core.utils import token_reader
+
+
+class Review(View):
+    @token_reader
+    def post(self, request):
+        review_data = json.loads(request.body)
+
+        title = review_data["title"]
+        content = review_data["uesrInput"]
+        user = request.user
