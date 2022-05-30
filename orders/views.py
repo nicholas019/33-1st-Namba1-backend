f6f706ad3f3b2f44d222bb4887d492eb67286d5c
sorkihg <sorkihg@gmail.com>
2022-05-30 23:02:53 +0900

    [Namba1팀]backend cart

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
+from itertools import product
+import json
+from django.views import View
+
+
+class Cart(View):
+    def post(self, request):
+        cart_data = json.loads(request.body)
+
+        quantity = cart_data["quntity"]
+        product_id = cart_data["product_id"]
+        user_id = request.user
+        
+        cart =
