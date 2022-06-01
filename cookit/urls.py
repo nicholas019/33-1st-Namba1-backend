e3cdd79a3115c4133c8ba08aaf7f4fa9eda99abe
sorkihg <sorkihg@gmail.com>
2022-06-01 14:22:53 +0900

    [Namba1팀]backend ProductsList관련 urls.py 일부수정

--- cookit/urls.py
+++ cookit/urls.py
@@ -3,5 +3,4 @@ from django.urls import path, include
 urlpatterns = [
     path('user', include('users.urls')),
     path('products', include('products.urls')),
-    path('main', include('products.urls')),
 ]
