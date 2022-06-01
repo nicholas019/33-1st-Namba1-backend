b2ce20508905deec04e6ea41799efeb64111d0a2
sorkihg <sorkihg@gmail.com>
2022-06-01 14:15:33 +0900

    [Namba1팀]backend ProductListView 수정

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from products.views import ProductListView, ProductDetailView, MainProductListView
+from products.views import ProductListView, ProductDetailView
 
 urlpatterns = [
     path('', ProductListView.as_view()),
     path('/<int:id>', ProductDetailView.as_view()),
-    path('/pc', MainProductListView.as_view()),
 ]
